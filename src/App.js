bceb577140cbeb3108a34c918dac0166a52d7ba4
Amit8830 <gamit9022@gmail.com>
2021-08-10 15:18:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import "./App.css";
 
 function App() {
+  const { register, handleSubmit, errors } = useForm();
+  const [userInfo, setUserInfo] = useState();
+  const onSubmit = (data) => {
+    setUserInfo(data);
+    console.log(data);
+  };
+  console.log(errors);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="container">
+    <pre>{JSON.stringify(userInfo, undefined, 2)}</pre>
+    <form onSubmit={handleSubmit(onSubmit)}>
 
+      <h1>Registration Form</h1>
+      <div className="ui divider"></div>
+      <div className="ui form">
+        <div className="field">
+
+          <label>Username</label>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            {...register("Username")}
+ />
+        </div>
+        <p>{errors?.username.message}</p>
+        <div className="field">
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            {...register("Email")}
+          />
+        </div>
+        <p>{errors?.email.message}</p>
+        <div className="field">
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            {...register("password")}
+          />
+        </div>
+        <p>{errors?.password.message}</p>
+        <button className="fluid ui button blue">Submit</button>
+      </div>
+    </form>
+  </div>
+);
+}
 export default App;
